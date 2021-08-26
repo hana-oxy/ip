dde4e562c1d3db1ea4623c863b424399eba755a0
hana-oxy <44062805+hana-oxy@users.noreply.github.com>
2021-08-26 14:34:31 +0800

    Update Duke.java
    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc;
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("        ____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("        ____________________________________________________________");
+        Scanner sc = new Scanner(System.in)
+        System.out.println(">");
+        String input = sc.nextLine();
+        System.out.println(input);
+        if (input == "bye" || input == "Bye") {
+            System.out.println("        ____________________________________________________________");
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("        ____________________________________________________________");
+        }
     }
 }
