8e8363466fbee6db47174a86c0bf8d7ccbae4eb2
hana-oxy <44062805+hana-oxy@users.noreply.github.com>
2021-08-27 16:49:12 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
         Scanner sc;
-        String[] list;
+        String[][] list = new String[][2];
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -9,20 +9,40 @@ public class Duke {
         System.out.println(">");
         String input = sc.nextLine();
         System.out.println(input);
-        if (input == "bye" || input == "Bye") {
-            System.out.println("        ____________________________________________________________");
-            System.out.println("Bye. Hope to see you again soon!");
-            System.out.println("        ____________________________________________________________");
-            return;
-        }
-        else if (input == "list") {
-            System.out.println("list");
-            System.out.println("        ____________________________________________________________");
-            System.out.println("        ____________________________________________________________");
-            for (int i = 0; i < list.length; i++) {
-                System.out.println(i+1 + "." + list[i]);
+        while (input != "bye" || input != "Bye") {
+            if (input == "list") {
+                System.out.println("list");
+                System.out.println("        ____________________________________________________________");
+                System.out.println("        ____________________________________________________________");
+                for (int i = 0; i < list.length; i++) {
+                    System.out.println(i+1 + "." + list[i]);
+                }
+                System.out.println("        ____________________________________________________________");
             }
-            System.out.println("        ____________________________________________________________");
+            else if (input.contains("done")) {
+                for (int i = 0; i < list.length(); i++){
+                    int listIndex = Integer.parseInt(input.substring(input.length() - 1)) - 1;
+                    if (i == listIndex) {
+                        list[listIndex][listIndex - 1] = "[x]";
+                        System.out.println("____________________________________________________________\n" +
+                                "     Nice! I've marked this task as done: \n" +
+                                list[listIndex][listIndex - 1] + listIndex[listIndex][listIndex] + "\n" +
+                                "    ____________________________________________________________");
+                    }
+                }
+            }
+            else {
+                for (int i = 0; i < list.length(); i++){
+                    if (list[i][i] == null) {
+                        list[i][i] = {{"[]", input}}
+                    }
+                }
+            }
         }
+
+        System.out.println("        ____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("        ____________________________________________________________");
+        return;
     }
 }
