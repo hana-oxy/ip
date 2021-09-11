b215c5226031368407242f76cdaf31944cc05bb4
hana-oxy <44062805+hana-oxy@users.noreply.github.com>
2021-09-11 22:41:16 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,48 +1,74 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        Scanner sc;
-        String[][] list = new String[][2];
+        int listRow = 100;
+        int listCol = 3;
+        String list[][][] = new String[listRow][listCol][listCol];
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        Scanner sc = new Scanner(System.in)
+        Scanner sc = new Scanner(System.in);
         System.out.println(">");
         String input = sc.nextLine();
         System.out.println(input);
-        while (input != "bye" || input != "Bye") {
+        if (input != null) {
+            // display list items
             if (input == "list") {
-                System.out.println("list");
-                System.out.println("        ____________________________________________________________");
-                System.out.println("        ____________________________________________________________");
+                System.out.println("____________________________________________________________");
                 for (int i = 0; i < list.length; i++) {
-                    System.out.println(i+1 + "." + list[i]);
+                    System.out.println(list[i][i][i] + list[i][i][i + 1] + list[i][i][i + 2]);
+                    System.out.println(i + 1 + "." + list[i]);
                 }
-                System.out.println("        ____________________________________________________________");
+                System.out.println("____________________________________________________________");
             }
+            // mark list items as done
             else if (input.contains("done")) {
-                for (int i = 0; i < list.length(); i++){
-                    int listIndex = Integer.parseInt(input.substring(input.length() - 1)) - 1;
+                for (int i = 0; i < list.length; i++){
+                    int listIndex = Integer.parseInt(input.substring(input.length - 1));
                     if (i == listIndex) {
-                        list[listIndex][listIndex - 1] = "[x]";
+                        list[listIndex][listIndex + 1][listIndex] = "[x]";
                         System.out.println("____________________________________________________________\n" +
-                                "     Nice! I've marked this task as done: \n" +
-                                list[listIndex][listIndex - 1] + listIndex[listIndex][listIndex] + "\n" +
-                                "    ____________________________________________________________");
+                                "Nice! I've marked this task as done: \n" +
+                                list[listIndex][listIndex][listIndex] + list[listIndex][listIndex] + "\n" +
+                                "____________________________________________________________");
                     }
                 }
             }
+            // create todo list
+            else if (input.contains("todo")) {
+                for (int i = 0; i < list.length; i++){
+                    int listIndex = Integer.parseInt(input.substring(input.size - 1)) - 1;
+                    if (i == listIndex) {
+                        list[listIndex][listIndex][listIndex] = "[T]";
+                        list[listIndex][listIndex + 1][listIndex] = "[ ]";
+                        list[listIndex][listIndex][listIndex + 2] = input.substring(4);
+                        System.out.println("____________________________________________________________\n" +
+                                "Got it. I've added this task: \n" +
+                                list[listIndex][listIndex][listIndex] +
+                                list[listIndex][listIndex + 1][listIndex] +
+                                list[listIndex][listIndex][listIndex + 2] + "\n" +
+                                "Now you have " + list.length + "tasks in the list." + "\n" +
+                                "____________________________________________________________");
+                    }
+                }
+            }
+            // add items to list
             else {
-                for (int i = 0; i < list.length(); i++){
+                for (int i = 0; i < list.length; i++){
                     if (list[i][i] == null) {
-                        list[i][i] = {{"[]", input}}
+                        list[i][i] = i + 1;
+                        list[i][i - 1] = input;
                     }
                 }
             }
-        }
 
-        System.out.println("        ____________________________________________________________");
+        }
+        
+
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("        ____________________________________________________________");
+        System.out.println("____________________________________________________________");
         return;
     }
 }
